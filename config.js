6511bb470e100dac4236e51b97aed72bfeac17ac
Quad-hir <107757976+Quad-hir@users.noreply.github.com>
2022-06-20 00:34:05 +0100

    Config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 /**
-   * Create By Dika Ardnt.
-   * Contact Me on wa.me/6288292024190
-   * Follow https://github.com/DikaArdnt
+   * Create By Quadhir.
+   * Contact Me on wa.me/2347082252014
+   * Follow https://github.com/Duxlin
 */
 
 const fs = require('fs')
@@ -18,9 +18,9 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','6281382420312','6288292024190']
-global.premium = ['6288292024190']
-global.packname = 'Hisoka Morrow'
+global.owner = ['2347082252014]
+global.premium = ['2347082252014']
+global.packname = 'Quadhirbot'
 global.author = 'WhatsApp Bot'
 global.sessionName = 'hisoka'
 global.prefa = ['','!','.','🐦','🐤','🗿']
